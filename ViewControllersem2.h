8cde4ba7c1027a1a80b775a65f6b05ab37fe290d
Sean Lim :D <Your Actual Email>
2014-10-01 22:16:47 +0800

    UPDATE Build 2.2.4 Fixed some bugs Added usability improvements Semester 2 half done , will complete

---
+++ ViewControllersem2.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllersem2.h
+//  Marks Calculator
+//
+//  Created by Sean Lim :D on 1/10/14.
+//  Copyright (c) 2014 Tangent.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllersem2 : UIViewController
+
+@end
---
+++ ViewControllersem2.m
@@ -0,0 +1,37 @@
+//
+//  ViewControllersem2.m
+//  Marks Calculator
+//
+//  Created by Sean Lim :D on 1/10/14.
+//  Copyright (c) 2014 Tangent.inc. All rights reserved.
+//
+
+#import "ViewControllersem2.h"
+
+@interface ViewControllersem2 ()
+
+@end
+
+@implementation ViewControllersem2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
